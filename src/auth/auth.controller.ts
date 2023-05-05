905a9ca461952991f43a0dcd28e0afaf39bc4eef
Aljaž Holobar <aljaz.holobar@gmail.com>
2023-05-05 09:11:11 +0200

    Auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,31 @@
-import {Controller, Post, UseGuards, Request, Get, Res} from '@nestjs/common';
 import {AuthService} from "./auth.service";
-import {AuthGuard} from "@nestjs/passport";
+import {Controller, Post, UseGuards, Request, Get, Res} from '@nestjs/common';
 import {Response} from "express";
+import {LocalAuthGuard} from "./guards/localAuth.guard";
+import {jwtAuthGuard} from "./guards/jwtAuth.guard";
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {
     }
 
-    @UseGuards(AuthGuard('local'))
+    @UseGuards(LocalAuthGuard)
     @Post('login')
     signIn(@Request() req, @Res() res: Response) {
         const jwt = req.user;
         res.setHeader('Set-Cookie',[jwt]).json();
+
     }
 
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(jwtAuthGuard)
     @Get('profile')
     profile(@Request() req) {
         return req.user;
+    }
+
+    @UseGuards(jwtAuthGuard)
+    @Post('logout')
+    logout(@Request() req){
+        req.res.setHeader("Set-Cookie", `Access_token=; HttpOnly; Path=/; Max-Age=0`);
     }
 }
