18e49b61ecf964d4af0929ad904fb1fa42f4e65e
Aljaž Holobar <aljaz.holobar@gmail.com>
2023-05-19 08:15:19 +0200

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173'
+  });
   app.use(cookieParser());
+
   app.useGlobalPipes(new ValidationPipe({whitelist:true}));
   await app.listen(3000);
 }
 bootstrap();
-
-
 
